3018b97f297e7d7d17e28f3e24071aea65f66808
nevilleantony98 <nevilleantony98@gmail.com>
2019-08-06 19:41:06 +0530

    desktop events page almost done

--- src/components/event-entry/index.js
+++ src/components/event-entry/index.js
@@ -3,23 +3,26 @@ import styles from './style.module.css';
 
 
 export default class EventEntry extends React.Component {
-    
+
     render() {
-        return(
-            <div className={styles["event-card"]}>
-                <div className = {styles["event-logo"]}>
-                    <a target="_blank"><img className={styles["logos"]} src={this.props.details.imgsrc}/></a>
-                </div>
-                <div className = {styles["event"]}>
-                    <div className = {styles["event-name"]}>
-                        <b>{this.props.details.name}</b>
+        return (
+            <div className={styles["event-container"]}>
+                <div className={styles["event-card"]}>
+                    <div className={styles["event-logo"]}>
+                        <a target="_blank"><img className={styles["logos"]} src={this.props.details.imgsrc} /></a>
                     </div>
-                    <div className = {styles["event-description"]}>
-                        {this.props.details.description}
+                    <div className={styles["event"]}>
+                        <div className={styles["event-name"]}>
+                            <b>{this.props.details.name}</b>
+                        </div>
+                        <div className={styles["event-description"]}>
+                            {this.props.details.description}
+                        </div>
                     </div>
+                    <button className={styles["learn-more"]}><a href="#">Learn More</a></button>
                 </div>
-                <button className={styles["learn-more"]}><a href="#">Learn More</a></button>
             </div>
+
         );
     }
 }
--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import asyncComponent from '../../utils/asyncComponent'
 
 import styles from './style.module.css'
+ //testing
+import ibetoLogo from '../../img/ibetologo.png'
+import hackfortomorrow from '../../img/hftlogo.png'
 
 const EventEntryImport = () => import('components/event-entry')
 const EventEntry = asyncComponent(EventEntryImport)
@@ -13,18 +16,42 @@ export default class Events extends React.Component {
             events: [
                 {
                     name: "IBeTo 2019",
-                    imgsrc: "hello",
+                    imgsrc: ibetoLogo,
                     link: "http://ibeto.excelmec.org/",
                     description: "Every social problem has a bright solution that needs to be found. IBeTo provides a platform to connect the brightest minds with these problems to create innovative solutions."
                 },
                 {
                     name: "HackForTomorrow 2019",
-                    imgsrc: "hello",
+                    imgsrc: hackfortomorrow,
+                    link: "https://hackfortomorrow.excelmec.org/",
+                    description: "A 24 hour hackathon which aims at using quick thinking to solve real-world problem statements in a competitive pressure environment. Coming Soon!"
+                },
+                {
+                    name: "HackForTomorrow 2019",
+                    imgsrc: hackfortomorrow,
+                    link: "https://hackfortomorrow.excelmec.org/",
+                    description: "A 24 hour hackathon which aims at using quick thinking to solve real-world problem statements in a competitive pressure environment. Coming Soon!"
+                },
+                {
+                    name: "HackForTomorrow 2019",
+                    imgsrc: hackfortomorrow,
+                    link: "https://hackfortomorrow.excelmec.org/",
+                    description: "A 24 hour hackathon which aims at using quick thinking to solve real-world problem statements in a competitive pressure environment. Coming Soon!"
+                },
+                {
+                    name: "IBeTo 2019",
+                    imgsrc: ibetoLogo,
+                    link: "http://ibeto.excelmec.org/",
+                    description: "Every social problem has a bright solution that needs to be found. IBeTo provides a platform to connect the brightest minds with these problems to create innovative solutions."
+                },
+                {
+                    name: "HackForTomorrow 2019",
+                    imgsrc: hackfortomorrow,
                     link: "https://hackfortomorrow.excelmec.org/",
                     description: "A 24 hour hackathon which aims at using quick thinking to solve real-world problem statements in a competitive pressure environment. Coming Soon!"
                 }
             ]
-        }   
+        }
     }
 
     render() {
@@ -35,14 +62,14 @@ export default class Events extends React.Component {
             grid.push(gridItem)
         }
         return (
-        <div>
-            <div className={styles["header"]}>
-                <h1 className={styles["title"]}>Excel 2019 Events!</h1>
+            <div>
+                <div className={styles["header"]}>
+                    <h1 className={styles["title"]}>Excel 2019 Events!</h1>
+                </div>
+                <div id={styles["event-grid"]}>
+                    {grid}
+                </div>
             </div>
-            <div id={styles["event-grid"]}>
-            {grid}
-        </div>
-        </div>
         )
     }
 }
