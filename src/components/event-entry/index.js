92e51c6ddf9262e6c1ffbe237658fd99be12ab76
nevilleantony98 <nevilleantony98@gmail.com>
2019-08-07 20:56:21 +0530

    events design

--- src/components/event-entry/index.js
+++ src/components/event-entry/index.js
@@ -6,19 +6,19 @@ export default class EventEntry extends React.Component {
 
     render() {
         return (
-            <div className={styles["event-container"]}>
-                <div className={styles["event-card"]}>
-                    <div className={styles["event-logo"]}>
-                        <a target="_blank"><img className={styles["logos"]} src={this.props.details.imgsrc} /></a>
+            <div className={styles["event-card"]}>
+                <div className={styles["event-logo"]}>
+                    <a target="_blank"><img className={styles["logos"]} src={this.props.details.imgsrc} /></a>
+                </div>
+                <div className={styles["event"]}>
+                    <div className={styles["event-name"]}>
+                        <b>{this.props.details.name}</b>
                     </div>
-                    <div className={styles["event"]}>
-                        <div className={styles["event-name"]}>
-                            <b>{this.props.details.name}</b>
-                        </div>
-                        <div className={styles["event-description"]}>
-                            {this.props.details.description}
-                        </div>
+                    <div className={styles["event-description"]}>
+                        {this.props.details.description}
                     </div>
+                </div>
+                <div className={styles["learnmore-container"]}>
                     <button className={styles["learn-more"]}><a href="#">Learn More</a></button>
                 </div>
             </div>
