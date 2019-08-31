bcda68ae735612dab371de5fe4ad254c7b9c9d9b
Xylegen <rohithsuresh719@gmail.com>
2019-08-31 22:57:32 +0530

    Made changes to contacts-event-card and contacts files

--- src/components/contacts-event-card/index.js
+++ src/components/contacts-event-card/index.js
@@ -3,53 +3,53 @@ import styles from './style.module.css';
 
 
 export default class EventCard extends React.Component {
-    
+
     render() {
         return(
-            <div className={styles["event-card"]}>
-                <div className={styles["event"]}>
-                    <div className={styles["event-image"]}>
-                        <div className = {styles["event-logo"]}>
-                            <a target="_blank"><img className={styles["logos"]} src={this.props.details.imgsrc}/></a>
-                        </div>
-                    </div>
-                    <div className={styles["event-in"]}>
-                        <div className = {styles["event"]}>                            
-                            <div className = {styles["event-name"]}>
-                                <b>{this.props.details.name}</b>
+            <div classname={styles["out"]}>
+                <div className={styles["event-card"]}>
+                    <div className={styles["event"]}>
+                        <div className={styles["event-image"]}>
+                            <div className = {styles["event-logo"]}>
+                                <a target="_blank"><img className={styles["logos"]} src={this.props.details.imgsrc}/></a>
                             </div>
-                            <div className={styles["descp"]}>
-                                <div className = {styles["event-description"]}>
-                                    {this.props.details.description}
-                                </div>
-                            </div>
-                            <div className={styles["contact-info"]}>
-                                <div className={styles["blue-square-container"]}>
-                                    <div className={styles["blue-square"]}>
-                                        <div className = {styles["event-email"]}>
-                                            <div className = {styles["event-email-img"]}>
-                                                <img  alt='email' src={require('../../img/message-closed-envelope.png')}/>
+                            <div className={styles["overlay"]}>
+                                <div className={styles["event-in"]}>
+                                    <div className = {styles["event"]}>
+                                        <div className={styles["contact-info"]}>
+                                            <div className = {styles["event-email"]}>
+                                                <div className = {styles["event-email-img"]}>
+                                                    <div className = {styles["event-email-id"]}>
+                                                        <img  alt='email' src={require('../../img/message-closed-envelope.png')}/><a href={"mailto:" + this.props.details.emailId}>{this.props.details.emailId}</a>
+                                                    </div>
+                                                </div>
                                             </div>
-                                            <div className = {styles["event-email-id"]}> 
-                                                <a href={"mailto:" + this.props.details.emailId}>{this.props.details.emailId}</a>
-                                            </div>                    
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className={styles["blue-square-container"]}>
-                                    <div className={styles["blue-square"]}>
-                                        <div className = {styles["event-tele"]}>
-                                            <img  alt='phone' src={require('../../img/phone-receiver.png')}/><a href={"tel:" + this.props.details.tele}>{this.props.details.tele}</a>                    
+                                            <div className = {styles["event-tele"]}>
+                                                <img  alt='phone' src={require('../../img/phone-receiver.png')}/><a href={"tel:" + this.props.details.tele}>{this.props.details.tele}</a>
+                                            </div>
+                                            <div className={styles["linkedin-image"]}>
+                                                <div className={styles["event-linkedin"]}>
+                                                    <a href={this.props.details.linkedin} target="_black"><img  alt='linkedin' src={require('../../img/linkedin-logo.png')}/></a>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>              
+                </div>
+                <div className= {styles["second"]}>
+                    <div className = {styles["event-name"]}>
+                        <b>{this.props.details.name}</b>
+                    </div>
+                    <div className={styles["descp"]}>
+                        <div className = {styles["event-description"]}>
+                            {this.props.details.description}
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
 }
-
-
--- src/pages/contacts/index.js
+++ src/pages/contacts/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import asyncComponent from '../../utils/asyncComponent'
 import styles from './style.module.css'
-import ibetoLogo from '../../img/ibetologo.png'
 import dhoni from '../../img/dhoni.jpeg'
 import hello from'../../img/hello.jpeg'
 
@@ -11,7 +10,7 @@ const EventCard = asyncComponent(EventCardImport)
 
 
 export default class ContactsPage extends React.Component {
-    
+
 
     constructor(props) {
         super(props)
@@ -22,16 +21,15 @@ export default class ContactsPage extends React.Component {
                     imgsrc: dhoni,
                     description:"Designation",
                     emailId:"msdhoni@gmail.com",
-                    tele:"+918592835329"
-                
-                
+                    tele:"+910000000000",
                 },
                 {
                     name: "MS Dhoni",
                     imgsrc: dhoni,
                     description: "Designation",
                     emailId: "msdhoni@gmail.com",
-                    tele: "+917907086032"
+                    tele: "+910000000000",
+                    linkedin: "www.hai.com",
                 },
                 {
                     name: "MS Dhoni",
@@ -61,7 +59,7 @@ export default class ContactsPage extends React.Component {
                     emailId:  "msdhoni@gmail.com",
                     tele: "+910000000000"
                 }
-                
+
             ]
         }
 
@@ -74,26 +72,23 @@ export default class ContactsPage extends React.Component {
             var gridItem = (<a target="_blank" key={i} className={styles["people"]} href={people[i].link} >
                 <EventCard details={
                 people[i]
-            } />            
-            </a>)       
+            } />
+            </a>)
             grid.push(gridItem)
         }
         return(
-            <div className={styles["contacts"]}>            
+            <div className={styles["contacts"]}>
                 <div className={styles["test-container"]}>
                     {/*<h1 className={styles["color-white-sayhello"]}>Say hello</h1>*/}
                     <img src={hello} className={styles["color-white-image"]}></img>
                     <p className={styles["color-white-dialogue"]}>Questions,kudos or karma...</p>
                     <p className={styles["color-white-dialogue"]}>Send a message to our Slack</p>
-                    <hr className={styles["hrline"]}/>                  
-                </div>                
-                <div id={styles["event-grid"]}>                    
+                    <hr className={styles["hrline"]}/>
+                </div>
+                <div id={styles["event-grid"]}>
                     {grid}
-                </div> 
-            </div>            
+                </div>
+            </div>
         );
     }
 }
-
-
-
