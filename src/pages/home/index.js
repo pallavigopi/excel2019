cdbb0322c8bd8f425101168f8ed7620f253a174b
nehamj <nehamj798@gmail.com>
2019-09-19 19:26:54 +0530

    demo ready :v:

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import $ from 'jquery'
 import Particles from 'react-particles-js'
 import asyncComponent from '../../utils/asyncComponent';
 import styles from './style.module.css'
+import './temp.css'
 import ibetoLogo from '../../img/ibetologo.png'
+import excellogo from '../../img/e.svg'
 import hackfortomorrow from '../../img/hftlogo.png'
 
 export default class Home extends React.Component {
@@ -26,11 +29,72 @@ export default class Home extends React.Component {
         }
     }
 
+    componentDidMount(){
+        $(document).on("mousemove",function(e) {  
+
+  var x = -($(window).innerWidth()/2- e.pageX)/30;
+  var y = ($(window).innerHeight()/2- e.pageY)/30;
+  $(".card").css({"transform": "rotateY("+x+"deg) rotateX("+y+"deg)"}) 
+
+ setTimeout(cityzoom, 1000);
+ function cityzoom() { $(".layer").css({"transform":"translate(0px)"}); } 
+
+
+});
+
+    }
+  
     render() {
 
         return (
-            <div>
-            </div>
+          <div id="parent">
+            <div id="h1">Excel 2019<br /><span style={{color:"grey",fontSize:"3vh"}}>Inspire Innovate Engineer</span></div> 
+
+
+
+<div id="god">
+<div className="card">
+  <div id="t1" className="text">290,00,0000 participants</div>
+  <img id="logo" src={require('../../img/newlogo.png')} />
+  <div id="t2" className="text">290 states</div>
+
+  <div id="c2" className="circle"></div>
+  <div id="c2a" className="circle"></div>
+
+  <div id="t3" className="text">290,000 partpants</div>
+
+  <div id="c5" className="circle"></div>
+  <div id="c6" className="circle"></div>
+  <div id="c7" className="circle"></div>
+  <div id="c8" className="circle"></div>
+
+  <div id="t4" className="text">180,0000 particats</div>
+
+</div>
+</div>
+<div id="h2">20th Edition<br /><span style={{color:"grey",fontSize:"3vh"}}>Govt. Model Engineering College</span></div>
+
+<div id="h3">Our Sponsors<br /><span style={{color:"grey",fontSize:"3vh"}}>MEC Excel 2019 Sponsors</span></div>
+
+
+<div id="grid">
+  <div id="sp1" className="sponsor">
+    <div className="t1">spnsor1</div>
+  </div>
+  <div id="sp2" className="sponsor">
+      <div className="t1">spnsor1</div>
+  </div>
+  <div id="sp3" className="sponsor">
+      <div className="t1">spnsor1</div>
+  </div>
+  <div id="sp4" className="sponsor">
+      <div className="t1">spnsor1</div>
+  </div>
+</div>
+
+</div>
+
+
         );
     }
 }
