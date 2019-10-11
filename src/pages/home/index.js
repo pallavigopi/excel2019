7af49f5a7b0b4685a5643deed84c3eafca31a20a
Suby <subincbenny7@gmail.com>
2019-10-11 18:01:44 +0530

    asdsad

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -135,7 +135,7 @@ export default class Home extends React.Component {
  Lorem Ipsum: usage Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It's also called placeholder (or filler) text. It's a convenient tool for mock-ups.
  Lorem Ipsum: usage Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It's also called placeholder (or filler) text. It's a convenient tool for mock-ups.
  as well as a random Lipsum generator. </div>
-
+ 
 <div id="grid2">
   
   <div>
