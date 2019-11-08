19603a3cfc19a20135b22af503217151029e5416
Nikhil713 <nikhilbabu213@gmail.com>
2019-11-08 23:30:24 +0530

    Add logos

--- src/pages/sponsors/index.js
+++ src/pages/sponsors/index.js
@@ -5,6 +5,20 @@ import townScript from '../../img/townscript.png'
 import litmus7 from '../../img/litmus7.png'
 import EventOz from '../../img/EVENTOZ-logo.png'
 import IndianExp from '../../img/TheNewIndianExp.png'
+import Nest from '../../img/nest.png'
+import Kimball from '../../img/kimbal.jpg'
+import Mirchi from '../../img/mirchi.jpg'
+import Cyrix from '../../img/cyrix.png'
+import CSL from '../../img/cochinshipyard.JPG'
+import Visualiq from '../../img/visualiq.png'
+import Bp from '../../img/bp.png'
+import Brawny from '../../img/brawny.png'
+import ThinkPalm from '../../img/thinkpalm.png'
+import Decalogo from '../../img/decalogo.jpg'
+import Escape from '../../img/escape.png'
+import Punnya from '../../img/punnya.png'
+import Winspire from '../../img/winspire.jpeg'
+import Onetv from '../../img/onetv.jpeg'
 
 export default class Sponsors extends Component {
     constructor(props) {
@@ -24,7 +38,7 @@ export default class Sponsors extends Component {
                 <div className={styles["sponsors-grid-container"]}>
                     <a href="https://litmus7.com/" target="_blank" className="sponsor-div">
                         <img className={styles["sponsor-img"]} src={litmus7} />
-                        <div className={styles["sponsor-title"]}>CS-Tech Sponsor</div>
+                        <div className={styles["sponsor-title"]}>LITMUS7<br />CS-Tech Sponsor</div>
                     </a>
                     <a href="https://www.townscript.com/" target="_blank" className="sponsor-div">
                         <img className={styles["sponsor-img"]} src={townScript} />
@@ -37,6 +51,63 @@ export default class Sponsors extends Component {
                     <a href="http://www.newindianexpress.com/" target="_blank" className="sponsor-div">
                         <img className={styles["sponsor-img-alt"]} src={IndianExp} />
                         <div className={styles["sponsor-title"]}>Media Partner</div>
+                    </a>
+                    <a href="http://www.nest.com/" target="_blank" className="sponsor-div">
+                        <img className={styles["sponsor-img"]} src={Nest} />
+                        <div className={styles["sponsor-title"]}>Refreshment Partner</div>
+                    </a>
+                    <a href="http://www.nest.com/" target="_blank" className="sponsor-div">
+                        <img className={styles["sponsor-img"]} src={Kimball} />
+                        <div className={styles["sponsor-title"]}>General Tech Partner</div>
+                    </a>
+                    <a href="http://www.nest.com/" target="_blank" className="sponsor-div">
+                        <img className={styles["sponsor-img"]} src={Mirchi} />
+                        <div className={styles["sponsor-title"]}>Radio Partner</div>
+                    </a>
+                    <a href="http://www.nest.com/" target="_blank" className="sponsor-div">
+                        <img className={styles["sponsor-img-alt"]} src={Cyrix} />
+                        <div className={styles["sponsor-title"]}>Lifeline Sponsor</div>
+                    </a>
+                    <a href="http://www.nest.com/" target="_blank" className="sponsor-div">
+                        <img className={styles["sponsor-img"]} src={CSL} />
+                        <div className={styles["sponsor-title"]}>Excel Associate</div>
+                    </a>
+                    <a href="http://www.nest.com/" target="_blank" className="sponsor-div">
+                        <img className={styles["sponsor-img"]} src={Visualiq} />
+                        <div className={styles["sponsor-title"]}>Visual IQ</div>
+                    </a>
+                    <a href="http://www.nest.com/" target="_blank" className="sponsor-div">
+                        <img className={styles["sponsor-img"]} src={Bp} />
+                        <div className={styles["sponsor-title"]}>Bharath Petrolium</div>
+                    </a>
+                    <a href="http://www.nest.com/" target="_blank" className="sponsor-div">
+                        <img className={styles["sponsor-img"]} src={Brawny} />
+                        <div className={styles["sponsor-title"]}>Brawny Brute</div>
+                    </a>
+                    <a href="http://www.nest.com/" target="_blank" className="sponsor-div">
+                        <img className={styles["sponsor-img"]} src={ThinkPalm} />
+                        <div className={styles["sponsor-title"]}>Think Palm</div>
+                    </a>
+                    
+                    <a href="http://www.nest.com/" target="_blank" className="sponsor-div">
+                        <img className={styles["sponsor-img"]} src={Decalogo} />
+                        <div className={styles["sponsor-title"]}>DECATHLON</div>
+                    </a>
+                    <a href="http://www.nest.com/" target="_blank" className="sponsor-div">
+                        <img className={styles["sponsor-img"]} src={Escape} />
+                        <div className={styles["sponsor-title"]}>Escape The Room, Kochi</div>
+                    </a>
+                    <a href="http://www.nest.com/" target="_blank" className="sponsor-div">
+                        <img className={styles["sponsor-img"]} src={Punnya} />
+                        <div className={styles["sponsor-title"]}>Punnya</div>
+                    </a>
+                    <a href="http://www.nest.com/" target="_blank" className="sponsor-div">
+                        <img className={styles["sponsor-img"]} src={Winspire} />
+                        <div className={styles["sponsor-title"]}>Winspire Academy</div>
+                    </a>
+                    <a href="http://www.nest.com/" target="_blank" className="sponsor-div">
+                        <img className={styles["sponsor-img"]} src={Onetv} />
+                        <div className={styles["sponsor-title"]}>ONE TV</div>
                     </a>
                 </div>
 
